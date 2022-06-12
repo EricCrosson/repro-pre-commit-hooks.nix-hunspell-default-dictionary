be5679ea77b65ad6bf4f606eb427ccdcab740be5
Eric Crosson <eric.s.crosson@utexas.edu>
2022-06-12 08:37:12 -0500

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "An example project.";
+
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, pre-commit-hooks, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              hunspell.enable = true;
+            };
+          };
+        };
+        devShell = nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      }
+    );
+}
